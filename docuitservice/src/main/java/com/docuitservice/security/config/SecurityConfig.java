ba1aff3d7872018c2fdca4280940d795e5ac534c
Ayyappan <ayyappan.chellapandi@gmail.com>
2023-10-13 13:17:43 +0530

    Swagger issue fixed

--- docuitservice/src/main/java/com/docuitservice/security/config/SecurityConfig.java
+++ docuitservice/src/main/java/com/docuitservice/security/config/SecurityConfig.java
@@ -19,6 +19,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.docuitservice.service.UserInfoUserDetailsService;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -35,14 +41,14 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http.csrf().disable().cors().and().authorizeHttpRequests()
+				.requestMatchers("/v3/api-docs", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
 				.requestMatchers("/auth/signUp", "/auth/verifyEmail", "/auth/resendCode", "/auth/pinGeneration",
 						"/auth/verifyMobileOtp", "/auth/login", "/auth/generateToken", "/auth/refreshToken",
 						"/auth/forgotPin", "/auth/verifyPin", "/auth/changePin", "/auth/adminLogin", "/auth/resendOtp")
 				.permitAll().requestMatchers("/auth/*").authenticated().requestMatchers("/family/*").authenticated()
 				.requestMatchers("/category/**").authenticated().requestMatchers("/dashboard/*").authenticated()
 				.requestMatchers("/document/*").authenticated().requestMatchers("/image/*").authenticated()
-				.requestMatchers("/swagger-resources/**", "/webjars/**", "/swagger-ui.html", "/v2/**").permitAll()
-				.requestMatchers("/api-docs/**").permitAll().requestMatchers("/actuator/health/**").permitAll().and()
+				.requestMatchers("/actuator/health/**").permitAll().and()
 				.authorizeHttpRequests().and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.authenticationProvider(authenticationProvider())
@@ -67,5 +73,17 @@ public class SecurityConfig {
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
 		return config.getAuthenticationManager();
 	}
+	
+	@Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Docuit Service").description("Docuit Service"))
+                .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+                .components(new Components()
+                        .addSecuritySchemes("Authorization", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")));
+    }
 
 }
