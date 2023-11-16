6b7ddfac1b1079746fbdd13373b81af135c490f2
Ayyappan <ayyappan.chellapandi@gmail.com>
2023-11-16 22:56:46 +0530

    category count issue fixed

--- docuitservice/src/main/java/com/docuitservice/repository/CategoryRepository.java
+++ docuitservice/src/main/java/com/docuitservice/repository/CategoryRepository.java
@@ -15,10 +15,14 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 	Category findById(String categoryId);
 
 	Category findByCategoryNameIgnoreCase(String categoryName);
-		
-	@Query("SELECT c.id, c.categoryName, c.status, COUNT(d.id) AS fileCount " + "FROM Category c "
-			+ "LEFT JOIN Document d ON c.id = d.category.id AND d.user.id = :userId AND d.documentStatus = true GROUP BY c.id")
+
+	@Query(value = "SELECT c.id, c.category_name, c.status, COUNT(d.id) AS fileCount " +
+            "FROM category c " +
+            "LEFT JOIN document d ON c.id = d.category_id AND (d.uploaded_by = :userId " +
+            "OR (d.id IN (SELECT document_id FROM share WHERE member_id IN " +
+            "(SELECT id FROM member WHERE user_id = :userId)) AND d.category_id = c.id)) " +
+            "GROUP BY c.id", nativeQuery = true)
 	List<Object[]> getCategoryDetailsWithFileCounts(@Param("userId") String userId);
-	
+
 
 }
